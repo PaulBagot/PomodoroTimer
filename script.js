3ba234cde48675319c9e7b4128325b8765e7b032
Paul Bagot <bagotpa@gmail.com>
2023-09-12 09:06:55 +0200

    added updateTime function

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 let button = document.getElementById('button');
 let timer = document.getElementById('timer');
+let workDisplay = document.getElementById('work_display');
+let pauseDisplay = document.getElementById('pause_display');
+let working = true;
 
-let minute = 25;
-let seconde = 0;
+let minute = 0;
+let seconde = 10;
 
 let decreaseCountdown = () => {
     if(minute >= 0) {
         if(!(minute == 0 & seconde == 0)) {
             minute = seconde == 0 ? minute - 1 : minute;
             seconde = seconde > 0 ? seconde - 1 : 59;
+        } else {
+            updateTime();
         }
     }
     let minuteDisplay = minute < 10 ? '0' + minute : minute;
     let secondeDisplay = seconde < 10 ? '0' + seconde : seconde;
     timer.textContent = minuteDisplay + ':' + secondeDisplay;
+}
+
+let updateTime = () => {
+    if(working) {
+        workDisplay.style.color = 'red';
+        pauseDisplay.style.color = 'green';
+        minute = 5;
+        seconde = 0;
+        working = false;
+        return;
+    }
+    minute = 25;
+    seconde = 0;
+    workDisplay.style.color = 'green';
+    pauseDisplay.style.color = 'red';
+    working = true;
 }
 
 
