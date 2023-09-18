d32d890f59bda530e465b0ebbb9c925f0d8f64d0
Paul Bagot <bagotpa@gmail.com>
2023-09-18 12:56:55 +0200

    added listener for work time

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 let buttonStart = document.getElementById('start');
 let buttonReset = document.getElementById('reset');
+
 let timer = document.getElementById('timer');
+
 let workDisplay = document.getElementById('work_display');
 let pauseDisplay = document.getElementById('pause_display');
 
+let workInput = document.getElementById('work_input');
+let pauseInput = document.getElementById('pause_input');
+
+
+let workMinutes = 25;
+let pauseMinutes = 5;
+
 let working = true;
 let launch = false;
-let minutes = 25;
+let minutes = workMinutes;
 let secondes = 0;
 let numberCicle = 0;
 let workColor = '#ff6666';
@@ -49,13 +58,13 @@ let updateMode = () => {
     if(working) {
         numberCicle++;
         if(numberCicle % 4 == 0)
-            minutes = 20;
+            minutes = pauseMinutes + 15;
         else
-            minutes = 5;
+            minutes = pauseMinutes;
         document.body.style.backgroundColor = pauseColor;
     } else {
         document.body.style.backgroundColor = workColor;
-        minutes = 25;
+        minutes = workMinutes;
     }
     working = working ? false : true;
     secondes = 0;
@@ -77,11 +86,9 @@ let onClickStart = () => {
     launch = true;
     workDisplay.style.color = pauseColor;
     pauseDisplay.style.color = workColor;
+    document.body.style.backgroundColor = workColor;
     buttonReset.style.display = 'block';
     buttonStart.style.display = 'none';
-    let timer = document.getElementById('contener_timer');
-    document.body.style.backgroundColor = workColor;
-    timer.style.display = 'inline-block';
 }
 
 /**
@@ -90,5 +97,14 @@ let onClickStart = () => {
 let onClickReset = () => {
     location.reload();
 }
+
+workInput.addEventListener('change', (event) => {
+    minutes = workInput.value
+    secondes = 0;
+    launch = false;
+    buttonReset.style.display = 'none';
+    buttonStart.style.display = 'block';
+})
+
 
 setInterval(pomodoro, 1000);
